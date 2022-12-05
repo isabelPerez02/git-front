845e86b2fb3e68800a7915a2507ad8d504910bdd
Isabel unab <maria.perez5@o365.unab.edu.co>
2022-12-05 09:43:00 -0500

    Example of components with images

---
+++ dino-front/src/components/header/Header.js
@@ -0,0 +1,10 @@
+import logo from '../../assets/img/Netflix.jfif'
+
+export const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt ="logo"/>
+            <h1>Bienvenido</h1>
+        </div>
+    )
+}
